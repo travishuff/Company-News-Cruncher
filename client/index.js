f3e31920cde73d9d84a9082f96996acaef8e8bdc
travishuff <huff.travis@gmail.com>
2016-12-17 12:28:52 -0800

    Feat: form submit on 'enter' keypress

--- client/index.js
+++ client/index.js
@@ -1,40 +1,53 @@
 $(document).ready((e) => {
 
-  $( '#message-button' ).on('click', () => {
-    let company = $( '#company' ).val();
+  $('#message-button').on('click', getAI);
+  $('#company').on('keypress', (e) => {
+    if (e.keyCode === 13) getAI();
+  });
+
+  function getAI() {
+    let company = $('#company').val();
+    $('#company').val('');
 
     $.ajax({
       method: "POST",
       url: "/getNews",
       data: company,
-      })
-      .done(msg => {
-        $( '.root' ).empty();
-        console.log(msg);
-        // write if staments to check for existence of concepts
-        $( ".root" ).append( `<p>Title: ${msg.title}</p>
-                              <p>Sentiment: ${msg.docSentiment.type}</p>
-                              <p>score: ${msg.docSentiment.score}</p>
-                              <p>concept 1: ${msg.concepts[0].text}</p>
-                              <p>relevance: ${msg.concepts[0].relevance}</p>` );
-      });
+    })
+    .done(msg => {
+      $('.root').empty();
+      console.log(msg);
+      // write if staments to check for existence of concepts
+      $(".root").append(`<p>Title: ${msg.title}</p>
+                            <p>Sentiment: ${msg.docSentiment.type}</p>
+                            <p>score: ${msg.docSentiment.score}</p>
+                            <p>concept 1: ${msg.concepts[0].text}</p>
+                            <p>relevance: ${msg.concepts[0].relevance}</p>`);
     });
+  }
 
-  $( '#ticker-button' ).on('click', () => {
-    let ticker = $( '#ticker' ).val();
+  $('#ticker-button').on('click', getTicker);
+  $('#ticker').on('keypress', (e) => {
+    if (e.keyCode === 13) getTicker();
+  });
+
+  function getTicker() {
+    let ticker = $('#ticker').val();
+    $('#ticker').val('');
 
     $.ajax({
       method: "POST",
       url: "/getTicker",
       data: ticker,
-      })
-      .done(msg => {
-        $( '.root2' ).empty();
-        let parsed = msg.slice(3);
-        parsed = JSON.parse(parsed);
-        console.log(parsed);
-        $( ".root2" ).append( `<p>${parsed[0].t} ${parsed[0].l}</p>
-                               <p class="ticker-time">${parsed[0].lt}</p>` );
-      });
-  });
+    })
+    .done(msg => {
+      $('.root2').empty();
+      let parsed = msg.slice(3);
+      parsed = JSON.parse(parsed);
+      console.log(parsed);
+      $(".root2").append(`<p>${parsed[0].t} ${parsed[0].l}</p>
+                              <p class="ticker-time">${parsed[0].lt}</p>`);
+    });
+  }
+
 });
